b027b1d625938bfc134ca2bd50fb12296e43f068
robert0oi <robertodesouzacamillo@gmail.com>
2025-03-19 11:45:45 -0300

    Adicionando conteúdo que não estava completo aula01

--- aula01/12032025aula01.py
+++ aula01/12032025aula01.py
@@ -1,19 +1,22 @@
-#Método que não possui parâmetros e nem retorno
+# retornar != print
+#Método que tem retorno e não recebe parâmetro
+def getPI():
+    return 3.14
+    
+#Método que não tem retorno e não recebe parâmetro
+def imprimirPI():
+    print( getPI() )
+
+#Método que tem retorno e recebe parâmetro
+def calcularAreaCirculo(raio):
+    area = getPI() * ( raio * raio )
+    return area
+
+#Método que não tem retorno e recebe parâmetro
 def imprimirNomeProfessor():
     print("Adalto")
 
 imprimirNomeProfessor()
-# retornar != print
-
-#Método
-
-
-#Método
-
-
-
-#Método
-
 
 class Carro:
     def __init__(self, modelo, ano, quilometragem):
