3b40b4b360bd5a0bec0b37ba66900bf25531080a
Roberto de Souza Camillo <153844542+robert0oi@users.noreply.github.com>
2025-05-28 13:59:30 -0300

    Arquivos Iniciais Lista Duplamente Encadeada - Aula 11

---
+++ PDF's Aula/aula11/atividadeAula11/ListaDuplamenteEncadeada.py
@@ -0,0 +1,6 @@
+from no import No
+
+class ListaDuplamenteEncadeada:
+    def __init__(self):
+        self.inicio = None
+        self.fim = None
---
+++ PDF's Aula/aula11/atividadeAula11/main.py
@@ -0,0 +1,2 @@
+from ListaDuplamenteEncadeada import ListaDuplamenteEncadeada
+
