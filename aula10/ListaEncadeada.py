ba51d716bda4cba7237bccb75a7625e297682e37
robert0oi <robertodesouzacamillo@gmail.com>
2025-05-28 11:43:10 -0300

    aula11 concluída - Lista Encadeada

--- aula10/ListaEncadeada.py
+++ aula10/ListaEncadeada.py
@@ -36,3 +36,49 @@ class ListaEncadeada:
             while aux: #ou aux is not None
                 print(aux.dado)
                 aux = aux.prox
+
+    def removerDoInicio(self):
+        if self.inicio is not None: #Verificar se a lista não está vazia.
+           # if self.inicio.prox == None: #Caso a lista tenha somente um elemento.
+                #self.inicio = None
+            #else: #Caso a lista tenha mais que um elemento.
+            self.inicio = self.inicio.prox ##atencao
+            print("Elemento Removido")
+        self.imprimir()
+
+
+    def removerDoFim(self):
+        if self.inicio is not None:
+            if self.inicio.prox == None:
+                self.inicio = None
+            else: #Cai aqui se tiver mais q um elemento
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux.prox: #while aux.prox != None: # Se o valor do último.próximo for nulo, o laço para.
+                    ant = aux
+                    aux = aux.prox
+                ant.prox = None # Aqui o penúltimo.próximo receberá None, já que queremos excluir o último. 
+            print("Elemento Removido")
+        self.imprimir()
+
+    def remover(self, valor): # Remover Específico
+        if self.inicio != None:
+            removeu = False
+            if self.inicio.dado == valor:
+                self.incio = self.incio.prox ##atencao
+                removeu = True
+            else:
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux != None: # while aux: (for um valor válido)
+                    if valor == aux.dado:
+                        ant.prox = aux.prox
+                        removeu = True
+                    else:
+                        ant = aux
+                        aux = aux.prox
+                if removeu:
+                    print(f"Elemento ${valor} removido")
+                else:    
+                    print("Elemento Não Encontrado")
+        self.imprimir()
--- aula10/main.py
+++ aula10/main.py
@@ -1,6 +1,17 @@
 from ListaEncadeada import ListaEncadeada
 lista = ListaEncadeada()
 lista.imprimir()
-lista.addNoInicio("Adalto")
+
 lista.addNoInicio("Abella")
+
+lista.addNoInicio("Adalto")
+
 lista.addNoFim("Cleber")
+
+lista.remover("João")
+print("-" * 30)
+lista.removerDoFim
+print("-" * 30)
+lista.removerDoInicio
+print("-" * 30)
+lista.imprimir()
