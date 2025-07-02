4b61e7f52685ef912e5509e693e50cb3a86bff08
robert0oi <robertodesouzacamillo@gmail.com>
2025-07-02 11:12:02 -0300

    aula15 - terminar atividade

---
+++ aula15/BuscaBinaria.py
@@ -0,0 +1,12 @@
+# Construa um algoritmo de busca contendo um vetor de números inteiros de 20 posições.
+# O algortimo deve ter duas funções, uma para busca sequencial e outra para busca binária.
+# Coloque um contador em cada função para contar as iterações de cada função.
+# Peça ao usuário que informe o valor que deseja procurar.
+# Então informe ao usuário se este valor existe no vetor e quantas iterações foram necessárias em cada função. 
+
+
+import random
+
+numerosAleatorios = [11, 78, 92, 1, 2, 34, 223, 65, 675, 987, 20, 30, 5, 7, 6, 77, 63, 55, 66, 76]
+random.shuffle(numerosAleatorios)
+
